02694f57f5b930dc86459de967f18154d24f7a73
Aiden Dickens <aiden.dickens@43540.auckland.natcoll.ac.nz>
2014-07-02 17:02:18 +1200

    commit

---
+++ js/jquery.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+// Do's and don't page
+		$('#text p').hide().delay(100).slideDown(2000);
+
+// Fonts page
+		$('#fonts h1').hide().delay(100).slideDown(2000);
+
+		$('#fonts h2').hide().delay(100).slideDown(2000);
+
+		$('#fonts h3').hide().delay(100).slideDown(2000);
+
+// color page
+		$('#color_text p').hide().delay(100).slideDown(2000);
+
+		$('#color_text li').hide().delay(100).slideDown(2000);
+
+// logo page
+		$('#logotext').hide().delay(100).slideDown(2000);
+
+});
