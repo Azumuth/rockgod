784eee2f253ddd98f3304589d581a6fa3f827c23
Aiden Dickens <aiden.dickens@43540.auckland.natcoll.ac.nz>
2014-07-17 14:12:44 +1200

    first commit

--- js/jquery.js
+++ js/jquery.js
@@ -10,11 +10,6 @@ $(document).ready(function(){
 
 		$('#fonts h3').hide().delay(100).slideDown(2000);
 
-// color page
-		$('#color_text p').hide().delay(100).slideDown(2000);
-
-		$('#color_text li').hide().delay(100).slideDown(2000);
-
 // logo page
 		$('#logotext').hide().delay(100).slideDown(2000);
 
